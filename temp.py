534c0a8443a00563bbd50060904d4acb3c956751
HimanshuPant10 <hhhimanshupant72@gmail.com>
2025-01-06 18:35:27 +0530

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,20 @@
+# import module
+import streamlit as st
+
+image_url = "https://i.cdn.newsbytesapp.com/images/l91120240815160849.png"
+
+# Title
+st.title("VE Dashboard")
+# Display the image
+st.image(image_url, width=500,caption="BIC", use_column_width=True)
+
+
+# Header
+st.header("Cell Design Team") 
+
+# Subheader
+st.subheader("Comparision Data")
+
+
+
+
